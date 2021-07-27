27038931447fdf8f534a0d17c153a946ee8cd07d
cactus1549 <cactus1549pool@gmail.com>
2021-07-27 18:17:03 +0200

    Fixes

---
+++ src/utils/coingecko.ts
@@ -0,0 +1,15 @@
+import * as fetch from "node-fetch";
+
+
+export let coingecko_request = async (ticker: string | undefined) => {
+    if (ticker === undefined) return undefined;
+    try{
+        const request = await fetch (`https://api.coingecko.com/api/v3/coins/${ticker}`);
+        const json = await request.json();
+        return json;
+    }catch (e){
+        console.log(e);
+        return undefined;
+    }
+ }
+
