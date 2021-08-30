de7ebc8a468fca8d1837d208f74e5a943d59014d
cactus1549 <cactus1549pool@gmail.com>
2021-08-30 19:12:00 +0200

    fix: negative number and 0 decimal places formatting

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,16 +3,17 @@ import { Utils } from "@arkecosystem/core-kernel";
 
 
 export const BigIntToString = (bigint: Utils.BigNumber | number, approx: number, decimals: number = 8) => {
+    if (approx > decimals) approx = decimals;
     let string = bigint.toString();
     if (string.length < decimals + 1 ) string = '0'.repeat(decimals + 1 - string.length) + string;
     if (approx > 0) return [string.slice(0, -decimals), ".", string.slice(-decimals, -(decimals-Math.min(approx, decimals)))].join("");
-    else return string.slice(0, -decimals);
+    else return string.slice(0, decimals > 0 ? -decimals : undefined);
 }
 
 export const BigIntToBString = (bigint: Utils.BigNumber | number, approx: number, decimals: number = 8) => {
     let string = BigIntToString(bigint, approx, decimals);
     let p = string.indexOf('.');
     if (p == -1) p = string.length;
-    for (let i = p - 4; i >= 0; i = i - 3) string = string.slice(0, i + 1) + "," + string.slice(i + 1);
+    for (let i = p - 4; i >= 0 && string[i] != "-"; i = i - 3) string = string.slice(0, i + 1) + "," + string.slice(i + 1);
     return string;
 }
