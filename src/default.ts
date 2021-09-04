503e3224a7aab4414bf7ad2c634f81904de08606
cactus1549 <cactus1549pool@gmail.com>
2021-09-04 21:22:04 +0200

    refactor: added dafaults

---
+++ src/default.ts
@@ -0,0 +1,10 @@
+export const defaults = {
+    //telegram_token: "",
+    delegate_name: "delegate",
+    base_delegates: 52,
+    base_voters: 25,
+    //"admin_id": [],
+    info:  "Hi, this is bot running as a core v3 plugin.\nThank you for trying it. :)",
+    links: "links:\nark.io",
+    ticker: "ark"
+};
