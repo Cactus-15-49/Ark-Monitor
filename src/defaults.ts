ce51434791027d9d412ece9c13d350959631fd89
cactus1549 <cactus1549pool@gmail.com>
2021-09-05 11:38:16 +0200

    fix: readd defaults.ts

---
+++ src/defaults.ts
@@ -0,0 +1,11 @@
+export const defaults = {
+    //telegram_token: "",
+    databaseName: "ark-monitor-bot",
+    delegate_name: "delegate",
+    base_delegates: 52,
+    base_voters: 25,
+    //"admin_id": [],
+    info:  "Hi, this is bot running as a core v3 plugin.\nThank you for trying it. :)",
+    links: "links:\nark.io",
+    ticker: "ark"
+};
