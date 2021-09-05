e2aebf0d4a92be747b96b38f2209eebdeff210ce
cactus1549 <cactus1549pool@gmail.com>
2021-09-05 12:12:04 +0200

    fix: handle coingecko http codes

--- src/utils/coingecko.ts
+++ src/utils/coingecko.ts
@@ -5,6 +5,9 @@ export let coingecko_request = async (ticker: string | undefined) => {
     if (ticker === undefined) return undefined;
     try{
         const request = await fetch (`https://api.coingecko.com/api/v3/coins/${ticker}`);
+        if (!request.ok){
+            return undefined;
+        }
         const json = await request.json();
         return json;
     }catch (e){
